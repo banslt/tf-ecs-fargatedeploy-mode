b570d7455bca86df593df09503810f78e0108eca
banslt <benjamin.anslot@protonmail.com>
2020-02-14 08:54:58 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+  default     = "us-east-1"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "440810850844.dkr.ecr.us-east-1.amazonaws.com/stresstestapp:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3100
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
